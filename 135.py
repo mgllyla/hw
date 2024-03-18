e966294ff2743d52d0ba2398cb5c5138a3542fa3
mgllyla <160612674+mgllyla@users.noreply.github.com>
2024-03-18 22:55:04 +0300

    Add files via upload

---
+++ 135.py
@@ -0,0 +1,4 @@
+n = int(input())
+mylst = list( range (n))
+mylst.insert(0, mylst.pop())
+print (mylst)
---
+++ vvv.py
@@ -0,0 +1,4 @@
+n = int(input())
+spisok = list(range(1, 10001))
+for i in reversed(spisok):
+    print(i, end="\n")
